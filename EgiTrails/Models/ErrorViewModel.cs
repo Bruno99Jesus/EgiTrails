f9d862f1ec7cd96de2c5854bd00d51fc74faa4a6
Bruno99Jesus <48434777+Bruno99Jesus@users.noreply.github.com>
2020-11-03 15:08:02 +0000

    Add project files.

---
+++ EgiTrails/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EgiTrails.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
