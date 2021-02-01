d0cee4d588cb1b2e5230ff0ad94201a72b896bd8
Bruno99Jesus <48434777+Bruno99Jesus@users.noreply.github.com>
2021-02-01 18:07:05 +0000

    Tabela guia

---
+++ EgiTrails/Models/GuiaTuristico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EgiTrails.Models
+{
+    public class GuiaTuristico
+    {
+        public int GuiaTuristicoId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        public int Telefone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
